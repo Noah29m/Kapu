063148766ce50778d2c164098f683b2e912944a6
HP <noahmuhindi11@gmail.com>
2023-04-04 08:55:50 +0300

    Created data, domain and presentation libraries.

--- app/src/main/java/com/noah/Kapu/MainActivity.kt
+++ app/src/main/java/com/noah/Kapu/MainActivity.kt
@@ -3,6 +3,7 @@ package com.noah.Kapu
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -28,5 +29,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Kapu() {
+    Column() {
+        
+    }
 
 }
